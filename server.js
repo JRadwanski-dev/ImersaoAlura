b24dc1b73a733cbdc9d5e9d5e81551facecdd7c9
José Radwanski <joseradwnski3@gmail.com>
2024-11-25 21:52:25 -0300

    Nem commit

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
-// Array de posts de exemplo (será substituído por dados do banco de dados)
-const posts = [
-    { descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150", id: 1 },
-    { descricao: "Gato fazendo yoga", imagem: "https://placekitten.com/400/200", id: 2 },
-    { descricao: "Gato fazendo panqueca", imagem: "https://placekitten.com/400/200", id: 3 },
-];
-
-// Cria uma instância do Express, que será o nosso servidor web
 const app = express();
-
+app.use(express.static("uploads"))
 routes(app)
 
-
-// Inicia o servidor na porta 3000
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
-
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -8,10 +8,13 @@ export default async function conectarAoBanco(stringConexao) {
     console.log('Conectando ao cluster do banco de dados...');
     await mongoClient.connect();
     console.log('Conectado ao MongoDB Atlas com sucesso!');
+
+    return mongoClient;
+    
   } catch (erro) {
     console.error('Falha na conexão com o banco!', erro);
     process.exit();
   }
 
-  return mongoClient;
+  
 }
